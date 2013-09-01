37c41f0a0b5e756a9d11c6198bd4b92b47e31c08
Frank Sautter <github@sautter.com>
2013-09-02 00:27:28 +0200

    new module to receive centronics printer data and forward it via tcp

---
+++ protocols/pport/pport_ecmd.c
@@ -0,0 +1,50 @@
+/*
+ *
+ * Copyright (c) 2010 by Erik Kunze <ethersex@erik-kunze.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License (either version 2 or
+ * version 3) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include <string.h>
+#include <avr/pgmspace.h>
+
+#include "config.h"
+#include "core/bit-macros.h"
+#include "core/debug.h"
+#include "protocols/pport/pport.h"
+
+#include "protocols/ecmd/ecmd-base.h"
+
+
+int16_t
+parse_cmd_pport_stats(char *cmd, char *output, uint16_t len)
+{
+  int16_t chars = snprintf_P(output, len,
+                             PSTR("rx bytes=%u, bf=%u, rt=%u"),
+                             pport_rx_bytes,
+                             pport_rx_bufferfull,
+                             pport_eth_retransmit);
+  return ECMD_FINAL(chars);
+}
+
+/*
+  -- Ethersex META --
+  block([[PPORT]] commands)
+  ecmd_ifdef(DEBUG_PPORT)
+    ecmd_feature(pport_stats, "pport stats",, Report statistic counters)
+  ecmd_endif()
+*/
